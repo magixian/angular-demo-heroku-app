2771dd36c72042e937b6d70fb3f8e23051f0fa30
magixian <magixian111@gmail.com>
2019-08-25 20:37:15 +0200

    update to deploy to circleci with tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-demo-heroku-app!');
+    expect(page.getTitleText()).toEqual('Welcome to Demo App');
   });
 });
